f7cabd42affd26e9d74ec0871d3630aab94e1378
nommelnommel-lab <guenkel19@gmail.com>
2026-01-22 11:01:10 +0100

    Fix app module imports

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,23 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { EventsModule } from './events/events.module';
+import { NewsModule } from './news/news.module';
 import { PermissionsModule } from './permissions/permissions.module';
-
-@Module({
-  controllers: [AppController],
-  imports: [EventsModule, PermissionsModule],
-codex/implement-warnings-crud-in-nestjs-api
 import { WarningsModule } from './warnings/warnings.module';
 
 @Module({
   controllers: [AppController],
-  imports: [EventsModule, WarningsModule],
-
-import { NewsModule } from './news/news.module';
-
-@Module({
-  controllers: [AppController],
-  imports: [EventsModule, NewsModule],
- main
+  imports: [EventsModule, NewsModule, PermissionsModule, WarningsModule],
 })
 export class AppModule {}
